73f5a5e175881f6835fe2a14e8ef5f78717e1130
DESKTOP-QH0HMQC\PC-13 <lass9436@gmail.com>
2021-03-18 09:49:25 +0900

    1교시 중간 저장

--- 2021_03_18.sql
+++ 2021_03_18.sql
@@ -22,8 +22,15 @@ LAST_DAY(***)
 MONTHS_BETWEEN
 SELECT ename, TO_CHAR(hiredate, 'yyyy/mm/dd HH24:MI:SS') hiredate,
        TRUNC(MONTHS_BETWEEN(SYSDATE, hiredate)) MONTH, --MONTHS_BETWEEN 의 경우 보통 딱 안 떨어져서 소수점을 표현하는데 이걸 쓸 때는 보통 소수점자리를 버리게된다.
-       ADD_MONTHS(SYSDATE, 5),
-       ADD_MONTHS(TO_DATE('2021-02-15','YYYY-MM-DD'), 5)
+       ADD_MONTHS(SYSDATE, 5) ADD_MONTHS,
+       ADD_MONTHS(TO_DATE('2021-02-15','YYYY-MM-DD'), -5) ADD_MONTHS2,--당연한 이야기지만 과거로도 갈 수 있다
+       NEXT_DAY(SYSDATE, 6) NEXT_FRIDAY, -- 일월화수목금토 -- 1234567
+       NEXT_DAY(SYSDATE, 1) NEXT_SUNDAY,
+       LAST_DAY(SYSDATE) LAST_DAY,
+       /*
+       * --SYSDATE를 이용하여 SYSDATE가 속한 월의 첫번째 날짜 구하기
+       */
+       TO_DATE(TO_CHAR(SYSDATE, 'YYYYMM') || '01','YYYYMMDD') FIRSTDAY
 FROM emp;
 
 SELECT TO_DATE('2021','YYYY') -- 이 경우 서버의 현재시간의 월과 그 월에 첫번째 날짜
@@ -31,3 +38,32 @@ FROM dual;
 
 SELECT TO_DATE('2021' || '0101' , 'YYYYMMDD')
 FROM dual;
+
+FUNCTION (date 종합 실습 fn3)
+파라미터로 yyyymm형식의 문자열을 사용하여 (ex: yyyymm = 201912)
+해당 년월에 해당하는 일자 수를 구해보세요
+
+yyyymm=201912 -> 31
+
+fn3] LAST_DAY(날짜)
+SELECT :yyyymm PARAM, TO_CHAR(LAST_DAY(TO_DATE(:yyyymm,'YYYYMM')),'DD') DT
+FROM dual;
+
+
+typecast
+형변환
+    - 명시적 형변환
+        TO_DATE, TO_CHAR, TO_NUMBER
+    - 묵시적 형변환
+
+SELECT *
+FROM emp
+WHERE empno = '7369';
+
+
+
+
+
+
+
+
